65c088913dd19521474d0eab59f57391fab7a11a
Andi <37834042+andi22032212@users.noreply.github.com>
2018-05-04 11:13:13 +0700

    Update Asset.java

--- src/main/java/com/wavesplatform/wavesj/Asset.java
+++ src/main/java/com/wavesplatform/wavesj/Asset.java
@@ -5,6 +5,8 @@ public abstract class Asset {
      * Constant used to represent WAVES token in asset transactions.
      */
     public static final String WAVES = "WAVES";
+    
+    public static final String NATA = "79jWQxTiV925jubY2c48vwJqVN2z1hU3rXX8uqdhuQnY";
 
     public static final long TOKEN = 100000000L;
 
